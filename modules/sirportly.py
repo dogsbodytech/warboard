e00a0808719218aac735d63431037a22c6e3a5ff
Alex Last <alex@dogsbodytechnology.com>
2016-01-15 10:21:05 +0000

    Created a new sirportly function to get the ticket data we need and imported the previous templates and static content from the old warboard.

---
+++ modules/sirportly.py
@@ -0,0 +1,25 @@
+import requests
+from config import sirportly_key, sirportly_token, sirportly_users, sirportly_endpoint, sirportly_red_filter
+from config import sirportly_total_filter, sirportly_reduser_filter, sirportly_greenuser_filter, sirportly_unassigned_filter
+
+def sirportly_filter(filterid, user):
+    if user != False:
+        endpoint = sirportly_endpoint+'/filter?filter='+filterid+'&user='+user
+    else:
+        endpoint = sirportly_endpoint+'/filter?filter='+filterid
+    try:
+        r = requests.get(endpoint, headers={'X-Auth-Token': sirportly_token, 'X-Auth-Secret': sirportly_key})
+        if r.status_code != requests.codes.ok:
+            raise requests.exceptions.RequestException
+        return(r.json()['pagination']['total_records'])
+    except requests.exceptions.RequestException:
+        return(0)
+
+def get_sirportly_data():
+    sirportly_data = {'users': {}}
+    sirportly_data['unassigned_tickets'] = sirportly_filter(sirportly_unassigned_filter, False)
+    sirportly_data['total_tickets'] = sirportly_filter(sirportly_total_filter, False)
+    sirportly_data['red_tickets'] = sirportly_filter(sirportly_red_filter, False)
+    for user in sirportly_users:
+        sirportly_data['users'][user+'_red'] = sirportly_filter(sirportly_reduser_filter, user)
+        sirportly_data['users'][user+'_green'] = sirportly_filter(sirportly_greenuser_filter, user)
