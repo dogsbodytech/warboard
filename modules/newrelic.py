7f927dc8ca42e705059823ef281ab30a79b54a87
Alex Last <alex@dogsbodytechnology.com>
2016-01-18 16:28:57 +0000

    Wrote code to return all the newrelic stats needed

--- modules/newrelic.py
+++ modules/newrelic.py
@@ -1,4 +1,4 @@
-import requests
+import requests, math
 from redis_functions import set_data, get_data
 from misc import log_errors, chain_results
 from config import newrelic_keys, newrelic_timeout, newrelic_endpoint
@@ -38,4 +38,26 @@ def get_newrelic_results():
     newrelic_results['total_newrelic_accounts'] = get_data('total_newrelic_accounts')
     newrelic_results['failed_newrelic'] = get_data('failed_newrelic')
     newrelic_results['checks'] = chain_results(all_results)
+    newrelic_results['total_checks'] = len(newrelic_results['checks'])
+    newrelic_results['green'] = 0
+    newrelic_results['red'] = 0
+    newrelic_results['orange'] = 0
+    newrelic_results['blue'] = 0
+    for check in newrelic_results['checks']:
+        if check['reporting'] == True:
+            check['orderby'] = max(check['summary']['cpu'], check['summary']['memory'], check['summary']['fullest_disk'], check['summary']['disk_io'])
+            if check['health_status'] == 'green':
+                newrelic_results['green'] +=1
+            elif check['health_status'] == 'orange':
+                newrelic_results['orange'] +=1
+            elif check['health_status'] == 'red':
+                newrelic_results['red'] +=1
+        elif check['reporting'] == False:
+            newrelic_results['blue'] +=1
+    newrelic_results['red_percent'] = math.ceil(100*float(newrelic_results['red'])/float(newrelic_results['total_checks']))
+    newrelic_results['green_percent'] = math.ceil(100*float(newrelic_results['green'])/float(newrelic_results['total_checks']))
+    newrelic_results['orange_percent'] = math.ceil(100*float(newrelic_results['orange'])/float(newrelic_results['total_checks']))
+    newrelic_results['blue_percent'] = 100-newrelic_results['red_percent']-newrelic_results['green_percent']-newrelic_results['orange_percent']
+    if newrelic_results['blue_percent'] < 0:
+        newrelic_results['green_percent'] = newrelic_results['green_percent']-abs(newrelic_results['blue_percent'])
     return(newrelic_results)
--- modules/results.py
+++ modules/results.py
@@ -1,34 +1,16 @@
 import redis
 from misc import log_errors
-from redis_con import redis_connect
 from pingdom import get_pingdom_results, store_pingdom_results
 from newrelic import get_newrelic_results, store_newrelic_results
 from sirportly import get_sirportly_results, store_sirportly_results
 
-rcon = redis_connect()
-
 def get_results():
-    pingdom_results = get_pingdom_results()
-    return(pingdom_results)
+    return(get_pingdom_results(), get_sirportly_results(), get_newrelic_results())
 
 def store_results():
     store_pingdom_results()
     store_newrelic_results()
     store_sirportly_results()
-
-def set_data(key, value):
-    try:
-        rcon.set(key, value)
-    except redis.exceptions.ConnectionError:
-        log_errors('Could not set '+key+' in Redis')
-
-def get_data(key):
-    try:
-        value = rcon.get(key)
-    except redis.exceptions.ConnectionError:
-        log_errors('Could not get '+key+' from Redis')
-        return(None)
-    return(value)
 
 if __name__ == '__main__':
     store_results()
