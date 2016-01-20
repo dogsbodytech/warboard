1d9e179d1224ecc20053ad52da1102b04993bcf0
Alex Last <alex@dogsbodytechnology.com>
2016-01-20 10:55:43 +0000

    Ignoring the log files in the gitignore and testing more calendar functions

--- modules/calendar_functions.py
+++ modules/calendar_functions.py
@@ -1,15 +1,18 @@
 import datetime, json
 from time import strftime
+from misc import log_errors
 from config import calendar_export
 from redis_functions import set_data, get_data, get_all_data
 
 calendar_split = '</li><li class="list-group-item list-group-item-info lead">' # This should only be changed when modifying the template
 
 def get_calendar_items():
-    calendar_items = get_all_data('calendar_*')
-    #old_date = item['start']['date']
-    #convert = datetime.datetime.strptime(old_date, '%Y-%m-%d')
-    #item['start']['date'] = convert.strftime('%a %d %B')
+    calendar_items = {}
+    calendar_keys = get_all_data('calendar_*')
+    for key in calendar_keys:
+        old_date = key.replace('calendar_', '')
+        convert = datetime.datetime.strptime(old_date, '%Y-%m-%d')
+    calendar_items[convert.strftime('%a %d %B')] = get_data(key)
     return(calendar_items)
 
 def store_calendar_items():
@@ -17,8 +20,11 @@ def store_calendar_items():
         c_data = json.load(c_file)
     c_file.close()
     for item in c_data['items']:
-        current = get_data('calendar_'+item['start']['date'])
-        if current == None:
-            set_data('calendar_'+item['start']['date'], item['summary'])
-        else:
-            set_data('calendar_'+item['start']['date'], current+calendar_split+item['summary'])
+        try:
+            current = get_data('calendar_'+item['start']['date'])
+            if current == None:
+                set_data('calendar_'+item['start']['date'], item['summary'])
+            else:
+                set_data('calendar_'+item['start']['date'], current+calendar_split+item['summary'])
+        except Exception as e: # Using this until support for times is added
+            log_errors('Could not parse calendar items')
