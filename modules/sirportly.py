8880bceb40e6d0175a253ea4f82fcab8da8328a3
Alex Last <alex@dogsbodytechnology.com>
2016-01-20 13:19:28 +0000

    Making sure the necessary sirportly numbers are ints

--- modules/sirportly.py
+++ modules/sirportly.py
@@ -57,14 +57,14 @@ def store_sirportly_results():
 
 def get_sirportly_results():
     sirportly_results = {'users': {}}
-    sirportly_results['unassigned_tickets'] = get_data('unassigned_tickets')
+    sirportly_results['unassigned_tickets'] = int(get_data('unassigned_tickets'))
     sirportly_results['red_percent'] = get_data('red_percent')
     sirportly_results['green_percent'] = get_data('green_percent')
-    sirportly_results['multiplier'] = get_data('multiplier')
+    sirportly_results['multiplier'] = int(get_data('multiplier'))
     sirportly_results['red_tickets'] = get_data('red_tickets')
     sirportly_results['total_tickets'] = get_data('total_tickets')
     for user in sirportly_users:
-        sirportly_results['users'][user+'_green'] = get_data(user+'_green')
-        sirportly_results['users'][user+'_red'] = get_data(user+'_red')
+        sirportly_results['users'][user+'_green'] = int(get_data(user+'_green'))
+        sirportly_results['users'][user+'_red'] = int(get_data(user+'_red'))
         sirportly_results['users'][user+'_total'] = get_data(user+'_total')
     return(sirportly_results)
