719ae39b2ef2fc915a768c25609792ec9e7d1c4d
Alex Last <alex@dogsbodytechnology.com>
2016-01-21 12:56:00 +0000

    Removed colon from the start date

--- modules/calendar_functions.py
+++ modules/calendar_functions.py
@@ -23,7 +23,7 @@ def store_calendar_items():
         if 'dateTime' in item['start']:
             item['start']['date'] = item['start']['dateTime'].split('T')[0]
             current_summary = item['summary']
-            start_time = datetime.datetime.strptime(item['start']['dateTime'].split('T')[1], '%H:%M:%SZ').strftime('%H:%M: ')
+            start_time = datetime.datetime.strptime(item['start']['dateTime'].split('T')[1], '%H:%M:%SZ').strftime('%H:%M')
             end_time = datetime.datetime.strptime(item['end']['dateTime'].split('T')[1], '%H:%M:%SZ').strftime('%H:%M: ')
             item['summary'] = start_time+'-'+end_time+current_summary
         current = get_data('calendar_'+item['start']['date'])
