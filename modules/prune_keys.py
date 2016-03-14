775b6a7cf299f52f53fba431edb70ce94d1cce79
Alex Last <alex@dogsbodytechnology.com>
2016-03-14 10:12:12 +0000

    Fixed mistake in the pruning function

--- modules/prune_keys.py
+++ modules/prune_keys.py
@@ -3,8 +3,8 @@ from redis_functions import get_all_data, delete_data
 
 def prune_old_keys():
     for key in get_all_data('pingdom_*'):
-        if key not in pingdom_keys:
+        if key.split('pingdom_')[1] not in pingdom_keys:
             delete_data(key)
     for key in get_all_data('newrelic_*'):
-        if key not in newrelic_keys:
+        if key.split('newrelic_')[1] not in newrelic_keys:
             delete_data(key)
