0a228ea057d15315ad48e6d01dfc9dc5fcc40553
Dan Benton <dan@dogsbody.org>
2016-10-31 20:06:20 +0000

    Shorten custom checks for better clarity

--- modules/pingdom.py
+++ modules/pingdom.py
@@ -53,6 +53,8 @@ def get_pingdom_results():
     pingdom_results['checks'] = chain_results(all_results) # Chain all the results together to be returned for the warboard
     pingdom_results['total_checks'] = len(pingdom_results['checks'])
     for check in pingdom_results['checks']: # Categorize all the checks as up/down etc
+        if check['type'] == 'httpcustom':
+            check['type'] = 'custom'
         if check['status'] == 'up':
             pingdom_results['pingdom_up'] +=1
         elif check['status'] == 'down':
