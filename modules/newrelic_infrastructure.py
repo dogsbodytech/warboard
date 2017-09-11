43f741e77a2e0a14e1651591fbdfa6488d723552
Jim <jim@dogsbodytechnology.com>
2017-09-11 17:26:51 +0100

    storing as json in redis

--- modules/newrelic_infrastructure.py
+++ modules/newrelic_infrastructure.py
@@ -97,7 +97,8 @@ def store_newrelic_infra_data():
                 infra_results['green'] += 1
 
             infra_results['successful_checks'] += 1
-            account_results.append(infrastructure_host)
+            json_infrastructure_host = json.dumps(infrastructure_host)
+            account_results.append(json_infrastructure_host)
 
         set_data('resources_newrelic_infra_'+account, account_results)
 
