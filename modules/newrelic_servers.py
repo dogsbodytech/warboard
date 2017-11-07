4aecf542564f904d91b1fb1ad07500de97b863d7
Jim <jim@dogsbodytechnology.com>
2017-11-07 17:07:57 +0000

    commit before time discussion

--- modules/newrelic_servers.py
+++ modules/newrelic_servers.py
@@ -1,8 +1,10 @@
 import requests
 import json
 import math
+import time
+import calendar
 from redis_functions import set_data, get_data
-from misc import log_messages, chain_results
+from misc import log_messages, chain_results, to_uuid
 from config import newrelic_servers_keys, newrelic_servers_timeout, newrelic_servers_endpoint
 
 def store_newrelic_servers_data():
@@ -11,7 +13,50 @@ def store_newrelic_servers_data():
     and stores it in redis as json with a key per server with value:
     '[{"orderby": 0, "timestamp": 0, "health_status": "green", "name": "wibble", "summary": {"cpu": 0, "fullest_disk": 0, "disk_io": 0, "memory": 0}}]'
     """
-    pass
+    nr_servers_results = {}
+    nr_servers_results['failed_newrelic_servers_accounts'] = 0
+    nr_servers_results['total_newrelic_servers_accounts'] = 0
+    nr_servers_results['total_checks'] = 0
+    nr_servers_results['successful_checks'] = 0
+    for account in newrelic_servers_keys:
+        try:
+            nr_servers_response = requests.get(newrelic_servers_endpoint, headers={'X-Api-Key': newrelic_servers_keys[account]}, timeout=newrelic_servers_timeout)
+            nr_servers_response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            nr_servers_results['failed_newrelic_servers_accounts'] += 1
+            log_messages('Could not get NewRelic Servers data for {} - error getting account data from api: Error: {}'.format(account, e), 'error')
+            continue
+
+        for server in json.loads(servers_response.text)['servers']:
+            nr_servers_host = {}
+            # servers returns name and host, if no dispay name is set it returns
+            # the host as both name and host
+            # I will crop the name in the jinja filter
+            nr_servers_host['name'] = server['name']
+            # servers which are not reporting have no health_status and no
+            # summary of metric data, hence we set them blue with orderby = 0
+            nr_servers_host['orderby'] = 0
+            nr_servers_host['health_status'] = 'blue'
+            # Convert from timestamp returned by newrelic api to milliseconds
+            # since epoch
+            # I've hardcoded the +00:00 since the newrelic servers api is only
+            # returning timestamps in UTC it isn't worth trying to accept times
+            # in any timezone and then convert them to UTC
+            nr_servers_host['timestamp'] = calendar.timegm(time.strptime(server['last_reported_at'], '%Y-%m-%dT%H:%M:%S+00:00')) * 1000
+            if server['reporting'] == True:
+                nr_servers_host['health_status'] = server['health_status']
+                nr_servers_host['summary'] = {
+                    'memory': server['memory'],
+                    'disk_io': server['disk_io'],
+                    'fullest_disk': server['fullest_disk'],
+                    'cpu': server['cpu']}
+                nr_servers_host['orderby'] = max(nr_servers_host['summary']['cpu'], nr_servers_host['summary']['memory'], nr_servers_host['summary']['fullest_disk'], nr_servers_host['summary']['disk_io']
+
+
+
+
+
+
 
 def get_newrelic_servers_data():
     newrelic_results = {}
