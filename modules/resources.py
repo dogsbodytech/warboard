835573e7927a2fb21000907006bf73298fddb5ef
Jim <jim@dogsbodytechnology.com>
2017-11-13 17:14:39 +0000

    fix the api

--- modules/resources.py
+++ modules/resources.py
@@ -100,6 +100,7 @@ def get_resource_results():
     else:
         resource_results['working_percentage'] = 0
 
+    resource_results['working_accounts'] = resource_results['total_accounts'] - resource_results['failed_accounts']
     return resource_results
 
 def clear_resources_keys():
