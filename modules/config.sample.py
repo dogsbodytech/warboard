c547b59982a11de3aa5d145811b0ee6434ac789e
Jim <jim@dogsbodytechnology.com>
2017-11-14 12:10:18 +0000

    up the batch size to improve performance

--- modules/config.sample.py
+++ modules/config.sample.py
@@ -78,7 +78,7 @@ influx_max_data_age = 300
 influx_timeout = 10
 # Used to reduce the number of queries required when a lot of databases are in
 # use
-influx_database_batch_size = 10
+influx_database_batch_size = 40
 
 # Sirportly details
 sirportly_endpoint = 'https://sirportly.example.org/api/v2/tickets'
