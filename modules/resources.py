2e94c31de94ec525c7e5edb40879a2e883d69328
Jim <jim@dogsbodytechnology.com>
2017-12-01 14:42:53 +0000

    put this in its own file so resources file doesn't need to know what modules are in use

--- modules/resources.py
+++ modules/resources.py
@@ -105,27 +105,3 @@ def get_resource_results():
 def clear_resources_keys():
     for key in get_all_data('resources:*'):
         delete_data(key)
-
-def list_unreporting_servers():
-    found_servers = set()
-    for key in get_all_data('resources:*'):
-        host_data = json.loads(get_data(key))[0]
-        found_servers.add(host_data['name'])
-
-    reporting_servers = set()
-    tick_data, tick_data_validity = get_tick_data()
-    for host in tick_data:
-        host_data = tick_data[host]
-        reporting_servers.add(host_data['name'])
-
-    newrelic_servers_data, newrelic_servers_data_validity = get_newrelic_servers_data()
-    for host in newrelic_servers_data:
-        host_data = newrelic_servers_data[host]
-        reporting_servers.add(host_data['name'])
-
-    newrelic_infra_data, newrelic_infra_data_validity = get_newrelic_infra_data()
-    for host in newrelic_infra_data:
-        host_data = newrelic_infra_data[host]
-        reporting_servers.add(host_data['name'])
-
-    return found_servers - reporting_servers
---
+++ modules/resources_list_unreporting_servers.py
@@ -0,0 +1,29 @@
+import json
+from redis_functions import get_all_data, get_data
+from newrelic_servers import get_newrelic_servers_data
+from newrelic_infrastructure import get_newrelic_infra_data
+from tick import get_tick_data
+
+def list_unreporting_servers():
+    found_servers = set()
+    for key in get_all_data('resources:*'):
+        host_data = json.loads(get_data(key))[0]
+        found_servers.add(host_data['name'])
+
+    reporting_servers = set()
+    tick_data, tick_data_validity = get_tick_data()
+    for host in tick_data:
+        host_data = tick_data[host]
+        reporting_servers.add(host_data['name'])
+
+    newrelic_servers_data, newrelic_servers_data_validity = get_newrelic_servers_data()
+    for host in newrelic_servers_data:
+        host_data = newrelic_servers_data[host]
+        reporting_servers.add(host_data['name'])
+
+    newrelic_infra_data, newrelic_infra_data_validity = get_newrelic_infra_data()
+    for host in newrelic_infra_data:
+        host_data = newrelic_infra_data[host]
+        reporting_servers.add(host_data['name'])
+
+    return found_servers - reporting_servers
