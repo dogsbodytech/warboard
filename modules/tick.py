a5d8ecfeb427edaf2fe1d1f6ad8914f75c0f8706
Jim <jim@dogsbodytechnology.com>
2018-03-08 15:33:11 +0000

    Add a query for deadman alerts. Decide if a server is reporting or not based on the deadman status rather than based on the fact it is missing data because we weren't accepting old data.  Log KeyErrors since they should be both rare and a concern and we are logging them in the prometheus module.

--- modules/tick.py
+++ modules/tick.py
@@ -55,7 +55,7 @@ def get_tick_data():
         # We actually want to pull this query from all of time since it
         # gives the most recent alert status however the db isn't going
         # to appreciate that to I'll grab the last 28 days for now
-        queries['alert_query'] = 'SELECT LAST("crit_duration") AS "crit_duration_before_alerting", LAST("warn_duration") AS "warn_duration_before_alerting" FROM "{}"."autogen"."kapacitor_alerts" WHERE time > now() - 28d GROUP BY "host","cpu","name","path"'
+        queries['alert_query'] = 'SELECT LAST("crit_duration") AS "crit_duration_before_alerting", LAST("warn_duration") AS "warn_duration_before_alerting", LAST("emitted") AS "deadman_status" FROM "{}"."autogen"."kapacitor_alerts" WHERE time > now() - 28d GROUP BY "host","cpu","name","path"'
         list_of_queries = []
 
         # The next two for loops are a little funky, we want to make as
@@ -66,6 +66,7 @@ def get_tick_data():
             # database is the list ["$database_name"], I can't see how
             # the list will have multiple values and would probably
             # rather break than loop through all of the returned values
+            assert len(database_as_list) == 1
             database = database_as_list[0]
             for query in queries:
                 list_of_queries.append(queries[query].format(database))
@@ -73,7 +74,6 @@ def get_tick_data():
         # Collect in a list incase influx_database_batch_size is not a
         # multipe of the number of queries we are running per server
         batches_response_list = []
-        time_accepted_since = ( time.time() - influx_max_data_age ) * 1000
         for beginning_of_slice in xrange(0, len(list_of_queries), influx_database_batch_size):
             batch_query = ';'.join(list_of_queries[beginning_of_slice:beginning_of_slice + influx_database_batch_size])
             try:
@@ -114,14 +114,30 @@ def get_tick_data():
                             alerts[hostname] = {}
                             alerts[hostname]['critical'] = [None]
                             alerts[hostname]['warning'] = [None]
+                            alerts[hostname]['deadman_alerting'] = False
 
                         for tag_or_field_position_in_list, tag_or_field in enumerate(each_measurement_with_an_alerting_status['columns']):
-                            if tag_or_field == "crit_duration_before_alerting":
+                            if tag_or_field == "deadman_status":
+                                assert len(each_measurement_with_an_alerting_status['values']) == 1
+                                # Checking if a deadman alert is up or
+                                # down isn't particularly clear.  We are
+                                # working on the basis that when the
+                                # latest value is 0 the server is down
+                                # and that any other value means the
+                                # server is reporting.  In my testing
+                                # this seems to be true for every alert
+                                # aside from the first one sent by a
+                                # each host.
+                                if each_measurement_with_an_alerting_status['values'][0][tag_or_field_position_in_list] == 0:
+                                    alerts[hostname]['deadman'] = True
+                            elif tag_or_field == "crit_duration_before_alerting":
                                 assert len(each_measurement_with_an_alerting_status['values']) == 1
                                 alerts[hostname]['critical'].append(each_measurement_with_an_alerting_status['values'][0][tag_or_field_position_in_list])
-                            if tag_or_field == "warn_duration_before_alerting":
+                            elif tag_or_field == "warn_duration_before_alerting":
                                 assert len(each_measurement_with_an_alerting_status['values']) == 1
                                 alerts[hostname]['warning'].append(each_measurement_with_an_alerting_status['values'][0][tag_or_field_position_in_list])
+                            else:
+                                log_messages('Unexpected tag or field when parsing kapacitor alerts for host \'{}\': {}'.format(hostname, tag_or_field), 'warning')
 
                     for hostname in alerts:
                         health_status = 'green'
@@ -131,6 +147,9 @@ def get_tick_data():
 
                         if max(alerts[hostname]['critical']) > 0:
                             health_status = 'red'
+
+                        if alerts[hostname][deadman_alerting]:
+                            health_status = 'blue'
 
                         if hostname not in hosts_data:
                             tick_data_validity['total_checks'] += 1
@@ -147,11 +166,6 @@ def get_tick_data():
                         hosts_data[hostname] = {}
                         hosts_data[hostname]['name'] = hostname
 
-                    # Check if we have old data
-                    if host_data['values'][0][0] < time_accepted_since:
-                        # No point storing old data since we don't store
-                        # old data from the servers module
-                        continue
                     if 'summary' not in hosts_data[hostname]:
                         hosts_data[hostname]['summary'] = {}
 
@@ -170,7 +184,8 @@ def get_tick_data():
                     tick_host_data['summary']['memory'],
                     tick_host_data['summary']['fullest_disk'],
                     tick_host_data['summary']['disk_io'])
-            except KeyError:
+            except KeyError as e:
+                log_messages('{} did not return data for all metrics, the first missing metric was {}'.format(host, e), 'warning')
                 tick_host_data['orderby'] = 0
                 tick_host_data['health_status'] = 'blue'
 
