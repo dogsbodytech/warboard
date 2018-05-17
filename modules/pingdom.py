37dc91143159453a9ac132fdcd949fe712f561b5
Jim <jim@dogsbodytechnology.com>
2018-05-17 09:12:19 +0100

    update variable name to what it is called elseware

--- modules/pingdom.py
+++ modules/pingdom.py
@@ -27,7 +27,7 @@ def get_pingdom_data():
                 r.raise_for_status()
             except requests.exceptions.RequestException:
                 logger.error('Could not get pingdom data for '+account)
-                pingdom_validity['failed_pingdom'] +=1
+                pingdom_validity['failed_accounts'] +=1
                 continue
 
             for check in r.json()['checks']:
