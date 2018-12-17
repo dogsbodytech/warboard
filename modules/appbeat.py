9e940b95e8b5f753ce48952e21aac91f28f83662
Jim <jim@dogsbodytechnology.com>
2018-12-17 10:53:49 +0000

    still syntax

--- modules/appbeat.py
+++ modules/appbeat.py
@@ -18,7 +18,7 @@ def get_appbeat_data_for_account(appbeat_key, timeout=20):
                         'Undefined' : 'down',
                         'Error': 'down',
                         'RuleMismatch': 'down',
-                        None, 'paused'}
+                        None: 'paused'}
     # This is actually returning a load of cool timestamps that it would
     # be nice for the warboard to use.
     # Since we want a validity time for all the accounts we would need
