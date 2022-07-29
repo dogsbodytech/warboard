4796e72dd7e3ea2d89cbf2a10d75e9b4ba01769c
Joel Ellis <joel@dogsbody.com>
2022-07-29 10:11:02 +0100

    Return aggregated results from redis & basic tables

---
+++ client/src/lib/percentLayer.svelte
@@ -0,0 +1,52 @@
+<!-- { filename: './components/Scatter.svelte' } -->
+<script type=ts>
+    // Import the getContext function from svelte
+    import { getContext } from 'svelte';
+  
+    // Access the context using the 'LayerCake' keyword
+    // Grab some helpful functions
+    const { colours, data, x, z } = getContext('LayerCake');
+  
+    // Customizable defaults
+    export let height = 24;
+
+
+    let percents: {
+        xDist: number,
+        width: number,
+        fill: string
+    }[] = [];
+
+    let w = 1;
+
+    $: {
+        let xDist = 0
+        percents = $data
+            // .sort((a, b) => {
+            //     if (a.name === b.name) {
+            //         return 0;
+            //     } else if (a.name > b.name) {
+            //         return 1;
+            //     } else {
+            //         return -1;
+            //     }
+			// 	})
+            .map((d: any)=> {
+                let i: any = {}
+                i.xDist = xDist;
+                i.width = $x(d)
+                i.fill = $z(d)
+                xDist += i.width
+                return i
+            })
+        w = xDist
+    }
+  </script>
+  
+  <g>
+    {#each percents as d, i}
+
+        <rect class="percentage-segment" fill="{d.fill}" {height} x="{d.xDist * 100 / w}%" y="0" width="{d.width * 100 / w}%"></rect>
+
+    {/each}
+  </g>
--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -9,16 +9,58 @@ client.on('error', (err) => console.log('Redis Client Error', err));
 
 
 export async function GET() {
+    let portmon_modules_checked = 0
+
     const portmon = (await Promise.all(
         (await client.keys("port_monitoring:*"))
             .map(async (mod) => { 
+                portmon_modules_checked += 1
                 let ret: any[] = JSON.parse(await client.get(mod) || '')
                 let dat = ret[0].map((i: any) => {i.mod = mod; return i});
                 return dat
             })
-            
         )).flat(1)
+    let portmon_ag_results = {
+        up: 0,
+        down: 0,
+        paused: 0,
+
+        total_accounts: 0, 
+        failed_accounts: 0,
+    };
+
+    (await client.keys("port_monitoring_success:*"))
+        .map(async (mod: string) => { 
+            let ret: any[] = JSON.parse(await client.get(mod) || '')
+            let dat = ret[0];
+            portmon_ag_results.up += dat.up
+            portmon_ag_results.down += dat.down
+            portmon_ag_results.paused += dat.paused
+            portmon_ag_results.total_accounts += dat.total_accounts
+            // console.log(mod, dat.valid_until < Date.now())
+            if (dat.valid_until < Date.now()) {
+                portmon_ag_results.failed_accounts += dat.total_accounts
+            } else {
+                portmon_ag_results.failed_accounts += dat.failed_accounts
+            }
+        })
     
+    let resmon_ag_results: any = {};
+
+    (await client.keys("resources_success:*"))
+        .map(async (mod: string) => { 
+            let ret: any[] = JSON.parse(await client.get(mod) || '')
+            let dat = ret[0];
+            resmon_ag_results.total_checks += dat.total_checks
+            resmon_ag_results.total_accounts += dat.total_accounts
+            
+            if (dat.valid_until < Date.now()) {
+                resmon_ag_results.failed_accounts += dat.total_accounts
+            } else {
+                resmon_ag_results.failed_accounts += dat.failed_accounts
+            }
+        })
+
     const resmon = await Promise.all(
         (await client.keys("resources:*"))
             .map(async (mod) => { 
@@ -31,6 +73,6 @@ export async function GET() {
         )
 
     return {
-        body: { portmon, resmon }
+        body: { portmon, resmon, portmon_ag_results, resmon_ag_results }
     };
 }
