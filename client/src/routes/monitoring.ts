ee87a5aa0f51ccc3712722e4ef5b2e81f14e3f31
Joel Ellis <joel@dogsbody.com>
2022-08-05 11:00:07 +0100

    Create index page and fix oauth

--- client/src/routes/monitoring.ts
+++ client/src/routes/monitoring.ts
@@ -1,5 +1,4 @@
 import { createClient } from 'redis';
-import { saveToken } from "./calendar/2AccountToken"
 
 const client = createClient()
 await client.connect()
@@ -10,19 +9,6 @@ import type { RequestEvent } from '@sveltejs/kit';
 
 /** @type {import('./__types/items').RequestHandler} */
 export async function GET(reqe: RequestEvent) {
-    let code = reqe.url.searchParams.get("code")
-    if (code) {
-
-        await saveToken(reqe.url.searchParams.get("state"), code)
-        // await saveToken(code)
-        return {
-            status: 303,
-            headers: {
-              location: `/calendar/2AccountToken?credentials=` + reqe.url.searchParams.get("state")
-            },
-            body: {  }
-        }
-    }
 
     let port_monitoring: any = {};
     let port_monitoring_success: any = {};
